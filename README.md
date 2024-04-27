d8498bc472ea11cb6b5f615a442728e003189cdf
DimRev <dimrev444@gmail.com>
2024-04-27 14:30:59 +0300

    Add formOpenQuestion function

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go CLI Form
+
+A quick form creator library
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var ANSI map[string]string
 
 func main() {
+	ANSI = map[string]string{
+		"cursor_up":   "\x1b[1A",
+		"delete_line": "\x1b[2K",
+	}
+
 	fmt.Println("Hello world")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Starting here:")
+
+	question_1 := "This is the first question"
+
+	_ = formOpenQuestion(scanner, question_1)
+
+}
+
+func formOpenQuestion(s *bufio.Scanner, q string) string {
+	var answer *string
+
+	fmt.Print("➜ ")
+	fmt.Print(q, ": ")
+
+	s.Scan()
+	input := s.Text()
+
+	answer = &input
+	if *answer == "" {
+		answer = nil
+	}
+
+	if answer != nil {
+		fmt.Print(ANSI["cursor_up"], ANSI["delete_line"], q, ": ", *answer, "\n")
+	} else {
+		fmt.Print(ANSI["cursor_up"], ANSI["delete_line"], q, ": ", "\n")
+	}
+
+	return *answer
 }
