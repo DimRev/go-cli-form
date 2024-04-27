cb60ba33205c8111df88b3b2c63f1205f7684205
DimRev <dimrev444@gmail.com>
2024-04-27 20:05:56 +0300

    Rewrote the Demo in main

--- main.go
+++ main.go
@@ -12,22 +12,30 @@ var LINEUP_CLEAR_LINE string
 
 func main() {
 
-	question_1 := "This is the first question"
-	question_2 := "This is the second question"
-	question_3 := "This is the third question"
-	question_4 := "This is the fourth question"
-	options1 := []string{"1", "2", "3", "4"}
-	options2 := []string{"yes", "no"}
-	options3 := []string{"hi", "121", "3", "cool", "5", "hello world", "7"}
+	question_1 := "What is your name"
+	question_2 := "What programming languages do you know"
+	question_3 := "How long have you been coding(years)"
+	question_4 := "What types of projects have you done (side projects included)"
+
+	options2 := []string{"JavaScript", "TypeScript", "GoLang", "Python", "Ruby", "Rust", "C", "C++", "C#", "Zig", "Lua"}
+	options3 := []string{"< 1", "1", "2", "3", "4", "5+"}
+	options4 := []string{"WebApp", "Bot", "Crawler", "CLI-Tool", "Game", "Mesh", "3D-Animation", "Embedded", "Back End Infra"}
 
 	fmt.Println(FORM_LOGO, "\n\n")
 
 	Form := form.Start()
-	Form.TextInput(question_1)
-	Form.SelectInput(question_2, options1)
-	Form.MultiSelectInput(question_3, options2)
-	Form.MultiSelectInput(question_4, options3)
+	res1 := Form.TextInput(question_1)
+	res2 := Form.MultiSelectInput(question_2, options2)
+	res3 := Form.SelectInput(question_3, options3)
+	res4 := Form.MultiSelectInput(question_4, options4)
 	Form.End()
+
+	fmt.Print(
+		"result1: ", res1, "\n",
+		"result2: ", res2, "\n",
+		"result3: ", res3, "\n",
+		"result4: ", res4, "\n",
+	)
 }
 
 var FORM_LOGO = fmt.Sprint("<!-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: -->\n",
